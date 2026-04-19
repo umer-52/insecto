24b99cf403085d04d33730f45d67a7168e17236c
Umer-52 <umerkhanpk03@gmail.com>
2026-04-19 17:09:15 +0500

    chore: setup github pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
-﻿import type { NextConfig } from "next";
+import type { NextConfig } from "next";
 import path from "path";
 
 const nextConfig: NextConfig = {
+  output: 'export',
+  basePath: '/insecto',
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
